02169c70d6e20e3dfe10ebc1f9c4696da06f24e3
sujan5655 <chaudharysujan5655@gmail.com>
2024-05-28 08:50:24 +0545

    question no 6 solution

---
+++ nameEndA.py
@@ -0,0 +1,12 @@
+#6. ⁠WAP that asks 10 user with their name and store them in a list. Print the name of only those users whose name ends with ‘a’ or ‘A’
+namelist=[]
+
+for name in range(10):
+    yourname=input("enter your name")
+    namelist.append(yourname)
+print("name of people end with 'a' or 'A'")
+for name in namelist:
+    if(name.lower().endswith('a')):
+        print(name)
+    else:
+        print()

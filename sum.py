4f73d7d90058ee240ca2c51f84e3469be2527132
sujan5655 <chaudharysujan5655@gmail.com>
2024-05-28 06:48:46 +0545

    question no 1 solution

---
+++ sum.py
@@ -0,0 +1,6 @@
+#1. Create a list of the first 10 positive integers. Write a for loop that calculates and prints the sum of all numbers in the list.
+numbers=[1,2,3,4,5,6,7,8,9,10]
+sum=0
+for i in range(11):
+  sum=sum+i
+print(sum) 
